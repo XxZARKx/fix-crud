7b35e295ab91d5361bec717bd33d3d04653dd7b7
xxzarkx <rodrigofranco.1301@gmail.com>
2024-11-30 00:01:55 -0500

    corrigiendo errores

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
-import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 # Configura tu conexión con FreeSQLDatabase
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = (
+    "mysql+pymysql://sql10748603:2lKyHhlCND@sql10.freesqldatabase.com/sql10748603"
+)
 
 # Crear el motor de conexión
 engine = create_engine(DATABASE_URL)
